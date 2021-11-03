841cf3832043e9a93b75dfaa306aaf354757ecb9
Dziq <arwu84@gmail.com>
2021-11-03 17:57:58 +0100

    major fixes

--- main/tile.go
+++ main/tile.go
@@ -14,27 +14,7 @@ components {
   }
 }
 embedded_components {
-  id: "tile_border"
-  type: "sprite"
-  data: "tile_set: \"/assets/tiles/tiles.atlas\"\n"
-  "default_animation: \"border1\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.1
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "tile_main"
+  id: "sprite"
   type: "sprite"
   data: "tile_set: \"/assets/tiles/tiles.atlas\"\n"
   "default_animation: \"tile1\"\n"
@@ -130,6 +110,53 @@ embedded_components {
   "  data: 50.0\n"
   "  data: 50.0\n"
   "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collide_tile"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"tile\"\n"
+  "mask: \"tile\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 55.0\n"
+  "  data: 55.0\n"
+  "  data: 0.5\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
